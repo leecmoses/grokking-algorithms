a6f4a055b37b63a80c79c02297bc0f6561f72c2f
Moses Lee <leecmoses@gmail.com>
2018-02-17 20:51:43 -0500

    Updated binary_search function.

--- 01-introduction-to-algorithms.py
+++ 01-introduction-to-algorithms.py
@@ -22,7 +22,7 @@ def binary_search(list, item):
     high = len(list)-1
 
     while low <= high:
-        mid = int((low + high) / 2)
+        mid = (low + high) // 2
         guess = list[mid]
         if guess == item:
             return mid

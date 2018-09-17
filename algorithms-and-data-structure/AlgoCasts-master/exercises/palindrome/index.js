c57c23e737b3f62dcf975c6e0c355db592f952d8
Moses Lee <leecmoses@gmail.com>
2018-09-17 14:04:18 -0400

    Review palindrome algo

--- algorithms-and-data-structure/AlgoCasts-master/exercises/palindrome/index.js
+++ algorithms-and-data-structure/AlgoCasts-master/exercises/palindrome/index.js
@@ -7,6 +7,24 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+/***************
+* Solution #1
+***************/
+// function palindrome(str) {
+//     const rev = str.split('').reverse().join('');
+//     return str === rev;
+// };
+
+
+/***************
+* Solution #2
+***************/
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i -1];
+    });
+};
+
 
 module.exports = palindrome;

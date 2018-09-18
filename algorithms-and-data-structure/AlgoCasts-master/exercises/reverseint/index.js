13d0bfc2cabfafed735075b95d0707bc30d3e257
Moses Lee <leecmoses@gmail.com>
2018-09-18 14:59:42 -0400

    Review reverse integer algo

--- algorithms-and-data-structure/AlgoCasts-master/exercises/reverseint/index.js
+++ algorithms-and-data-structure/AlgoCasts-master/exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/***************
+* Solution #1
+***************/
+// function reverseInt(n) {
+//     const rev = n.toString().split('').reverse().join('');
+//     if (n < 0) {
+//         return parseInt(rev) * -1;
+//     } else {
+//         return parseInt(rev);
+//     }
+// };
+
+
+/***************
+* Solution #2
+***************/
+function reverseInt(n) {
+    const rev = n.toString().split('').reverse().join('');
+    return parseInt(rev) * Math.sign(n);
+};
+
 
 module.exports = reverseInt;

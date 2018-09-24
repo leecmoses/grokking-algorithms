dc0671527e4fb9f8de8b3d6309f28a6c51f6df99
Moses Lee <leecmoses@gmail.com>
2018-09-24 17:10:14 -0400

    Reviewed anagram algorithms

--- algorithms-and-data-structure/AlgoCasts-master/exercises/anagrams/index.js
+++ algorithms-and-data-structure/AlgoCasts-master/exercises/anagrams/index.js
@@ -8,6 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/***************
+* Solution #1
+***************/
+// \w in a regExp ignores anything that are not characters
+// Helper function is used to create charMaps for strA and str B
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) { // tests to see if same number of types of characters
+//         return false;
+//     }
+    
+//     for (let char in aCharMap) { // compares the frequency of the characters
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {};
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
+
+
+/***************
+* Solution #2
+***************/
+// .sort() method will try to meaningfully sort the order of the elements
+
+// function anagrams(strA, strB) {
+//     return cleanStr(strA) === cleanStr(strB);
+// };
+
+// function cleanStr(str) {
+//     return str.replace(/[^\w]/g, '').toLowerCase.split('').sort().join('');
+// };
 
 module.exports = anagrams;
